9523c69452e3e4d2969d13b0abb6260733898c36
itsyoru <sstaminup97@gmail.com>
2025-02-07 20:51:45 -0500

    added animations + geolocator, working app

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -10,7 +10,7 @@ class Weather {
 
   factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
-      cityName: json['main'],
+      cityName: json['name'],
       temperature: json['main']['temp'].toDouble(),
       mainCondition: json['weather'][0]['main'],
     );
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:weather_app/models/weather_model.dart';
+import 'package:weather_app/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +11,74 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // API Key
+  final _weatherService = WeatherService('641dca11463acf71f2eb8d36412d640b');
+  Weather? _weather;
+
+  // Fetch Weather
+  _fetchWeather() async {
+    // Get current city
+    String cityName = await _weatherService.getCurrentCity();
+
+    // Get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // Animations
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/sunny.json'; // Default to Sunny
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/clouds.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/rain.json';
+      case 'thunderstorm':
+        return 'assets/thunder.json';
+      case 'clear':
+        return 'assets/sunny.json';
+      default:
+        return 'assets/sunny.json';
+    }
+  }
+
+  // Initial App State
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? "loading city..."),
+            Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+            Text("${_weather?.temperature.round()}°C"),
+            Text(_weather?.mainCondition ?? "")
+          ],
+        ),
+      ),
+    );
   }
 }
